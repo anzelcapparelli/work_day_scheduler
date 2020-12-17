b921e5b222c99705004add2f86a04b5dbda53554
Anzel Capparelli <anzelcapparelli@gmail.com>
2020-12-16 21:17:38 -0800

    added files to add javascript to; added empty javascript file

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+
+
+
+
+
+
+});        // makes sure that html fully loads before running javascript
+            // this ensures javascript doesn't try to add/change something to an element that doesn't yet exist
