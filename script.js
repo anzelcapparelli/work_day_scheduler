b93f3d1518ae5af88c8e6e58c70ffbd7087663e5
Anzel Capparelli <anzelcapparelli@gmail.com>
2020-12-16 23:51:26 -0800

    parsed the hour divs with moment; they are now ready to be compared to actual time

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-$(function(){
+$(function () {
 
-$("#currentDay").text(moment().format("dddd, MMM Do"))
+    $("#currentDay").text(moment().format("dddd, MMM Do"))      //current day
 
-//adding classes past, present, and future
-//can get time with $(".hour").value
-//forEach function, compare with actual time
-//if actual is later, addClass("past"), removeClass("present future")
-//if equals, addClass("present"), removeClass("past future")
-//if actual is earlier, addClass("future"), removeClass("past present")
+    var hours_array = moment($(".hour").text(), "hh a");
+
+    // for (var i = 0; i < hours_array.length; i++){
+
+    //     hours_array[i] = parseInt(hours_array[i]);
+
+    // } 
+
+    console.log(hours_array);
+
+    moment()
+
+    var hours = parseInt($(".hour").text());
+    console.log(hours);
+    //adding classes past, present, and future
+    //can get time with $(".hour").value
+    //forEach function, compare with actual time
+    //if actual is later, addClass("past"), removeClass("present future")
+    //if equals, addClass("present"), removeClass("past future")
+    //if actual is earlier, addClass("future"), removeClass("past present")
 
 
 
 
-$(".saveBtn").on("click", function(){
+    $(".saveBtn").on("click", function () {
 
 
-    var test = this.previousElementSibling.value;       //stores value in text area upon clicking the save
-    console.log(test);
+        var test = this.previousElementSibling.value;       //stores value in text area upon clicking the save
+        console.log(test);
 
 
-})
+    })
 
 
 
